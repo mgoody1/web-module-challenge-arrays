092171d09b9bf0d2d2a54ec426733ad75e7934b7
Mark Goodwin <magoodwin502@gmail.com>
2021-04-27 18:32:23 -0400

    5 test passed

--- index.js
+++ index.js
@@ -45,10 +45,14 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
+
+
+function copy(array){
+    const flavorCopy = array;
+    return flavorCopy;
 }    
 
+console.log(copy(originalFlavors));
 
 
 
@@ -64,9 +68,14 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(array){
+//    console.log(array.length);
+//    if array === 31;
+//    console.log(true);
+//    return array.length
 }
+
+// console.log(is31Flavors(originalFlavors));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -81,9 +90,14 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
-}
+function addFlavor(array, string){
+    array.unshift(string);
+    return array;
+ }
+ 
+ 
+ console.log(addFlavor(originalFlavors, 'Rainbow Sherbert'));
+ 
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -97,9 +111,12 @@ Use the removeLastFlavor function below to do the following:
     For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
-   /*your code here*/
-}
+function removeLastFlavor(array){
+    array.pop();
+    return array;
+  }
+  
+  console.log(removeLastFlavor(originalFlavors));
 
 
 
@@ -114,9 +131,12 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
-}
+function getFlavorByIndex(array, index){
+    let arrInd = array[index];
+    return arrInd;
+  }
+  
+  console.log(getFlavorByIndex(originalFlavors, 2));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
